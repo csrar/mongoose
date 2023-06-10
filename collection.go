0cc54f8dd97a3853361662ea15310a0b23bda198
Cesar Arguello <guille50@gmail.com>
2023-06-09 23:35:59 -0600

    added updateOne and findOne (#1)

--- collection.go
+++ collection.go
@@ -10,6 +10,8 @@ import (
 //go:generate mockgen -source=collection.go -destination=./mocks/collection.go -package=mocks
 type CollectionInterface interface {
 	InsertOne(ctx context.Context, document interface{}, opts ...*options.InsertOneOptions) (*mongo.InsertOneResult, error)
+	UpdateOne(ctx context.Context, filter interface{}, update interface{}, opts ...*options.UpdateOptions) (*mongo.UpdateResult, error)
+	FindOne(ctx context.Context, filter interface{}, opts ...*options.FindOneOptions) *mongo.SingleResult
 }
 
 type CollectionHelper struct {
@@ -24,4 +26,14 @@ func NewCollection(collectionName string, database DatabaseInterface) Collection
 
 func (col CollectionHelper) InsertOne(ctx context.Context, document interface{}, opts ...*options.InsertOneOptions) (*mongo.InsertOneResult, error) {
 	return col.collection.InsertOne(ctx, document)
+}
+
+func (col CollectionHelper) UpdateOne(ctx context.Context, filter interface{}, update interface{},
+	opts ...*options.UpdateOptions) (*mongo.UpdateResult, error) {
+	return col.collection.UpdateOne(ctx, filter, update, opts...)
+}
+
+func (col CollectionHelper) FindOne(ctx context.Context, filter interface{},
+	opts ...*options.FindOneOptions) *mongo.SingleResult {
+	return col.collection.FindOne(ctx, filter, opts...)
 }
